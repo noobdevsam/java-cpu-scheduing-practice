50c2499e8d30e0e6335322e57053cb6909434daf
noobdevsam <mdshoykotislam05@gmail.com>
2025-08-25 01:51:50 +0600

    add detailed output for process statistics in TablePrinter

--- src/util/TablePrinter.java
+++ src/util/TablePrinter.java
@@ -37,6 +37,36 @@ public class TablePrinter {
             totalWidth += w + 3; // 3 for padding and separator
         }
         String line = "-".repeat(Math.max(totalWidth, 10));
+
+        // Print header
+        System.out.println(line);
+        for (int i = 0; i < headers.length; i++) {
+            System.out.printf("%-" + widths[i] + "s | ", headers[i]);
+        }
+        System.out.println();
+        System.out.println(line);
+
+        // Print rows in provided order
+        double sumTAT = 0;
+        double sumWT = 0;
+        for (ProcessStats p : processes) {
+            System.out.printf("%-" + widths[0] + "d | ", p.getPid());
+            System.out.printf("%-" + widths[1] + "d | ", p.getArrivalTime());
+            System.out.printf("%-" + widths[2] + "d | ", p.getBurstTime());
+            System.out.printf("%-" + widths[3] + "d | ", p.getCompletionTime());
+            System.out.printf("%-" + widths[4] + "d | ", p.getTurnaroundTime());
+            System.out.printf("%-" + widths[5] + "d | ", p.getWaitingTime());
+            System.out.println();
+            sumTAT += p.getTurnaroundTime();
+            sumWT += p.getWaitingTime();
+        }
+
+        System.out.println(line);
+
+        double averageTAT = sumTAT / processes.size();
+        double averageWT = sumWT / processes.size();
+        System.out.printf("Average Turnaround Time: %.2f\n", averageTAT);
+        System.out.printf("Average Waiting Time: %.2f\n", averageWT);
     }
 
     private static void updateWidths(
