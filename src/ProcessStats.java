7705d78fd7f017fcdb822a7865a2099aa50190bf
noobdevsam <mdshoykotislam05@gmail.com>
2025-08-25 00:01:04 +0600

    add completion time calculations to ProcessStats class

--- src/ProcessStats.java
+++ src/ProcessStats.java
@@ -1,8 +1,17 @@
 public class ProcessStats extends ProcessInput {
 
+    public int completionTime;
+    public int turnaroundTime;
+    public int waitingTime;
+
     public ProcessStats(ProcessInput input) {
         super(input.getPid(), input.getArrivalTime(), input.getBurstTime());
     }
 
+    public void setCompletionTime(int completionTime) {
+        this.completionTime = completionTime;
+        this.turnaroundTime = this.completionTime - getArrivalTime();
+        this.waitingTime = this.turnaroundTime - getBurstTime();
+    }
 
 }
